3702ea903f933db746c66dd37d0b0e59d03c09c9
Edinson Fernandez <eyemps.23@gmail.com>
2024-09-18 02:25:10 -0500

    Update

--- resources/views/home/main.blade.php
+++ resources/views/home/main.blade.php
@@ -4,10 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Devix Solutions Software</title>
-    <link rel="icon" href="{{ asset('/public/favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('/public/Icono.ico') }}" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/rayos-animated.css') }}">
-    <style>
+     <style>
         /* Animaciones */
         @keyframes fadeInUp {
             from {
