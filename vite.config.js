6f8275a64d26a2264ef415b2454411cf9c0d6bde
Edinson Fernandez <eyemps.23@gmail.com>
2024-09-18 03:47:38 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,16 +8,9 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    build: {
-        manifest: true, // Esto es necesario para Laravel en producción
-        outDir: 'public/build', // Usa una ruta relativa sin la barra inicial
-        rollupOptions: {
-            output: {
-                // Coloca los assets correctamente
-                entryFileNames: 'assets/[name].js',
-                chunkFileNames: 'assets/[name].js',
-                assetFileNames: 'assets/[name].[ext]'
-            },
-        },
+    build:{
+        manifest:true,
+        outDir: 'public/build'
+
     },
 });
