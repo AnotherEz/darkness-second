3c3ec4e9a7ddaad814e4c7af6a7b0d96b237d800
Edinson Fernandez <eyemps.23@gmail.com>
2024-09-18 15:11:16 -0500

    Agrega GitHub

--- resources/views/home/sections/contacto.blade.php
+++ resources/views/home/sections/contacto.blade.php
@@ -8,7 +8,7 @@
 
 <div class="bg-gray-200 py-8 sm:py-12 lg:py-16">
     <div class="container mx-auto px-4">
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
             <!-- WhatsApp -->
             <div class="bg-white rounded-lg shadow-lg p-6 animate-fadeInUp" style="animation-delay: 0.2s;">
                 <h3 class="text-lg sm:text-xl font-semibold mb-2">WhatsApp</h3>
@@ -33,6 +33,15 @@
                 <p class="text-gray-600 mb-4">Síguenos en Instagram y mantente actualizado con nuestras novedades.</p>
                 <a href="https://www.instagram.com/anotherez23" target="_blank" class="bg-pink-500 hover:bg-pink-600 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110 flex items-center justify-center">
                     <i class="fab fa-instagram mr-2"></i> Visítanos
+                </a>
+            </div>
+
+            <!-- GitHub -->
+            <div class="bg-white rounded-lg shadow-lg p-6 animate-fadeInUp" style="animation-delay: 0.8s;">
+                <h3 class="text-lg sm:text-xl font-semibold mb-2">GitHub</h3>
+                <p class="text-gray-600 mb-4">Explora mis proyectos y contribuciones en GitHub.</p>
+                <a href="https://github.com/AnotherEz" target="_blank" class="bg-gray-800 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110 flex items-center justify-center">
+                    <i class="fab fa-github mr-2"></i> Ver Perfil
                 </a>
             </div>
         </div>
